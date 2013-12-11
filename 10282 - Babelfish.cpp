a4fac9014920f774eee065d5ab16eede952b66aa
Sakib Sami <s4kibs4mi@gmail.com>
2013-12-11 23:25:34 +0600

    Create 10282 - Babelfish.cpp

---
+++ 10282 - Babelfish.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    map <string , string> mp;
+    string a,b;
+    while(getline(cin , a)){
+		if(a.length()==0) break;
+		b.assign(a,0,a.find(" "));
+		a.erase(0,a.find(" ")+1);
+        mp[a] = b;
+    }
+
+    while(getline(cin , a)){
+		if(a.length()==0) break;
+        if(mp.find(a)!=mp.end())
+            cout << mp[a] << endl;
+        else cout << "eh" << endl;
+    }
+    return 0;
+}
